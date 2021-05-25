0933ed332896503339291084eaaf4639cdd3c10c
Sachin Sharma <nicsachin007@gmail.com>
2021-05-25 12:26:22 +0530

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
 echo "deployment started......."
-
-cp -r build/. /home/sachin/WebstormProjects/portfolio-website-hosted
-echo "build copied ....."
-cd ..
-cd portfolio-website-hosted
-echo "inside folder ....."
-git add .
-git commit -m "$(date)"
+npm run build
 git push origin master
 echo "deployment completed ....."
